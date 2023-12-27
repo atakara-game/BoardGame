b4ec61d2ba3cd2302d395213f809d52869ca9ae3
AnoGame <atakara.game@gmail.com>
2023-12-27 17:07:22 +0900

    一部処理を修正

--- Assets/Atakara/BoardGame/Scripts/Tile.cs
+++ Assets/Atakara/BoardGame/Scripts/Tile.cs
@@ -10,14 +10,10 @@ public class Tile : MonoBehaviour
     [SerializeField]
     private Color _originalColor; // 元の色を保存
 
-    [SerializeField]
-    private Material _originalMaterial; // 元の色を保存
-
     void Start()
     {
         _tileRenderer = GetComponent<Renderer>();
         _originalColor = _tileRenderer.material.color; // 元の色を保存
-        _originalMaterial = _tileRenderer.material; // 元の色を保存
     }
 
     public void ChangeColor(Color newColor)
@@ -28,7 +24,6 @@ public class Tile : MonoBehaviour
     public void ResetColor()
     {
         _tileRenderer.material.color = _originalColor;
-        // _tileRenderer.material = _originalMaterial;
     }
 
 }
